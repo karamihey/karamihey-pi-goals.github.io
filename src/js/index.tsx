b51e9758067f464ea238e5aec4112f704a3b3c0e
Karina Mihey <karina.mihey@gmail.com>
2019-09-06 14:54:52 +0300

    Init React application

---
+++ src/js/index.tsx
@@ -0,0 +1,10 @@
+// polyfills
+import 'react-app-polyfill/ie11';
+import 'react-app-polyfill/stable';
+// libraries
+import React from 'react';
+import ReactDOM from 'react-dom';
+// components
+import App from 'components/App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
