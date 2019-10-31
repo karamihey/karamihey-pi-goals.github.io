43a67b65cd213eb85d65ce09ca7999cb3ae7ad66
karamihey <karamihey@users.noreply.github.com>
2019-10-31 17:09:31 +0000

    Deploying to gh-pages from staging fb04523eccebad2ce4bf98b46817dd96599c9c39

---
+++ precache-manifest.c42ffa9d3d11c87c5f2c1d3d53ad97d9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c760e9ad7d82c7ea6fdb7285f872867d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3af6e6bd52c7c2b71fa9",
+    "url": "/static/css/main.6e1781b2.chunk.css"
+  },
+  {
+    "revision": "3ac49928267ff57a2a2d",
+    "url": "/static/js/2.748ebef0.chunk.js"
+  },
+  {
+    "revision": "3af6e6bd52c7c2b71fa9",
+    "url": "/static/js/main.0bd6e8f4.chunk.js"
+  },
+  {
+    "revision": "c3f596a217e315983efa",
+    "url": "/static/js/runtime~main.136b562b.js"
+  }
+]);
